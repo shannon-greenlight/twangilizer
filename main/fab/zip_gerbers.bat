a9bf95a78de03932fc1a6b9a49dab49740f4e310
shannon-greenlight <shannon@greenlightdevelopment.com>
2024-01-17 09:32:01 -0800

    Init checkin

---
+++ main/fab/zip_gerbers.bat
@@ -0,0 +1,8 @@
+@echo off
+set board=main
+echo Creating gerbers for: %board%
+del .\%board%_gerbers.zip
+"C:\Program Files\7-Zip\7z.exe" a .\%board%_gerbers.zip .\gerber\*.gbr
+"C:\Program Files\7-Zip\7z.exe" a .\%board%_gerbers.zip .\gerber\*.gbrjob
+"C:\Program Files\7-Zip\7z.exe" a .\%board%_gerbers.zip .\gerber\*.drl
+pause
